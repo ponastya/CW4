d0d9c8250b9b68f0504e0b510f482be87d16e76c
ponastya <nastya8po@gmail.com>
2022-12-05 17:13:12 +0300

    2. Создайте модель и схему пользователя и добавьте к ней CRUD (views с методами GET/POST/PUT).

---
+++ service/user.py
@@ -0,0 +1,25 @@
+from dao.user import UserDAO
+
+
+class UserService:
+    def __init__(self, dao: UserDAO):
+        self.dao = dao
+
+    def get_one(self, bid):
+        return self.dao.get_one(bid)
+
+    def get_all(self):
+        return self.dao.get_all()
+
+    def get_by_username(self, username):
+        return self.dao.get_by_username(username)
+
+    def create(self, user_d):
+        return self.dao.create(user_d)
+
+    def update(self, user_d):
+        self.dao.update(user_d)
+        return self.dao
+
+    def delete(self, rid):
+        self.dao.delete(rid)
