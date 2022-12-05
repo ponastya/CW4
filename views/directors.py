fa424172f49a72138a883e3facd7f1adf1f23f85
ponastya <nastya8po@gmail.com>
2022-12-05 17:31:59 +0300

    Шаг 3. Добавьте недостающие методы

--- views/directors.py
+++ views/directors.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restx import Resource, Namespace
 
 from dao.model.director import DirectorSchema
@@ -14,9 +15,25 @@ class DirectorsView(Resource):
         return res, 200
 
 
+    def post(self):
+        req_json = request.json
+        director = director_service.create(req_json)
+        return "", 201, {"location": f"/directors/{director.id}"}
+
 @director_ns.route('/<int:rid>')
 class DirectorView(Resource):
     def get(self, rid):
         r = director_service.get_one(rid)
         sm_d = DirectorSchema().dump(r)
         return sm_d, 200
+
+    def put(self, rid):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = rid
+        director_service.update(req_json)
+        return "", 204
+
+    def delete(self, rid):
+        director_service.delete(rid)
+        return "", 204
